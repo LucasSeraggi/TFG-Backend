fb60265cff2bf861be7c7d4a4780de8f7e61689d
Paulo Paiva <paulo.paiva@tech4h.com.br>
2023-02-25 15:36:01 -0300

    Database Connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,14 +1,23 @@
+const { Client } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-  database: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    // connection: pgp(process.env.DB_URL)
+
+  async dbConnect () {
+    const config = {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: true
+    }
+
+    const client = new Client(config);
+    await client.connect();
+
+    return client;
   }
 }
