a7f2feb540429404dc64bac18f5b4748f4ae4243
Jackson Galdino <jacksongdas.jackson@gmail.com>
2023-05-12 18:56:14 -0300

    Fechando Conexão com o banco de dados - Utilizando um debounce de 1min;

--- src/config/databaseConnection.config.ts
+++ src/config/databaseConnection.config.ts
@@ -12,14 +12,42 @@ const dbConfig = {
   ssl: process.env.DB_SSLMODE ? true : false,
 }
 
+let clientSql: Client | null;
+let timer: NodeJS.Timeout | null = null;
+
+function debounce(
+  fn: Function,
+  s: number
+): void {
+  if (timer) {
+    timer.refresh();
+  } else {
+    timer = setTimeout(() => {
+      timer = null;
+      fn();
+    }, s * 1000);
+  }
+}
+
 export = {
   dbConfig,
 
   async dbConn(query: any) {
     console.info(query);
-    const client = new Client(dbConfig);
-    await client.connect();
 
-    return client.query(query);
+    debounce(() => {
+      let temp = clientSql;
+      clientSql = null;
+
+      temp?.end();
+      console.info('Connection sql closed');
+    }, 60);
+
+    if (!clientSql) {
+      clientSql = new Client(dbConfig);
+      await clientSql.connect();
+    }
+
+    return clientSql!.query(query);
   }
 }
