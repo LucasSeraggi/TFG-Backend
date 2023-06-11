e4da52032e1f102b627cac4902bb5e8f22ebcf8c
Lucas Seraggi <lucas.seraggi@hotmail.com>
2023-06-11 15:45:59 -0300

    news routes

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -8,4 +8,8 @@ router.get('/api/news', [verifyToken], newsController.find);
 
 router.post('/api/news/register', [verifyToken], newsController.register);
 
+router.put('/api/news/update', [verifyToken], newsController.update)
+
+router.delete('/api/news/delete', [verifyToken], newsController.remove)
+
 export = router;
